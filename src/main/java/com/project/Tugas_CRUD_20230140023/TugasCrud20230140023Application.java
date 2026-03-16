f7ad462a0d1db1f8e7553aba512e863af7cc40a0
Kevin Adrian <kevin19305.ib@gmail.com>
2026-03-16 11:53:51 +0700

    file default dari spring boot

---
+++ src/main/java/com/project/Tugas_CRUD_20230140023/TugasCrud20230140023Application.java
@@ -0,0 +1,13 @@
+package com.project.Tugas_CRUD_20230140023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Tugas_CRUD_20230140023/TugasCrud20230140023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Tugas_CRUD_20230140023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud20230140023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
