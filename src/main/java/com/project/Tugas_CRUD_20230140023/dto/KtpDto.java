579aa49701f6ca55fbdc375b794255766a093702
Kevin Adrian <kevin19305.ib@gmail.com>
2026-03-16 11:51:51 +0700

    membuat file Berisi DTO untuk codenya

---
+++ src/main/java/com/project/Tugas_CRUD_20230140023/dto/KtpDto.java
@@ -0,0 +1,15 @@
+package com.project.Tugas_CRUD_20230140023.dto;
+
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class KtpDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
