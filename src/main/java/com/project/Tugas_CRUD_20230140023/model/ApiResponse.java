d28c13b23c8c3fd498f56ac1aa72de66d0866c7a
Kevin Adrian <kevin19305.ib@gmail.com>
2026-03-16 11:47:37 +0700

    file untuk membuat format api response

---
+++ src/main/java/com/project/Tugas_CRUD_20230140023/model/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.project.Tugas_CRUD_20230140023.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+}
